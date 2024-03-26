864c5e62c7a81117365f7867ae3d66fc1b24e722
eduds08 <edudscrc@outlook.com>
2024-03-26 12:11:41 -0300

    changed bindCommand methods to, in the future, implement key rebinding

--- Batali/InputHandler.h
+++ Batali/InputHandler.h
@@ -42,52 +42,59 @@ public:
 
 	void bindRunRightCommand(sf::Keyboard::Scancode button)
 	{
-		m_runRightCommand = new RunCommand{};
+		if (m_runRightCommand == nullptr)
+			m_runRightCommand = new RunCommand{};
 		m_runRightCommand->button = button;
 	}
 
 	void bindRunLeftCommand(sf::Keyboard::Scancode button)
 	{
-		m_runLeftCommand = new RunCommand{};
+		if (m_runLeftCommand == nullptr)
+			m_runLeftCommand = new RunCommand{};
 		m_runLeftCommand->button = button;
 	}
 
 	void bindJumpCommand(sf::Keyboard::Scancode button) 
 	{
-		m_jumpCommand = new JumpCommand{};
+		if (m_jumpCommand == nullptr)
+			m_jumpCommand = new JumpCommand{};
 		m_jumpCommand->button = button; 
 	}
 
 	void bindAttack1Command(sf::Keyboard::Scancode button)
 	{
-		m_attack1Command = new Attack1Command{};
+		if (m_attack1Command == nullptr)
+			m_attack1Command = new Attack1Command{};
 		m_attack1Command->button = button;
 	}
 
 	void bindAttack2Command(sf::Keyboard::Scancode button)
 	{
-		m_attack2Command = new Attack2Command{};
+		if (m_attack2Command == nullptr)
+			m_attack2Command = new Attack2Command{};
 		m_attack2Command->button = button;
 	}
 
 	void bindRollCommand(sf::Keyboard::Scancode button)
 	{
-		m_rollCommand = new RollCommand{};
+		if (m_rollCommand == nullptr)
+			m_rollCommand = new RollCommand{};
 		m_rollCommand->button = button;
 	}
 
 	void bindUltimateCommand(sf::Keyboard::Scancode button)
 	{
-		m_ultimateCommand = new UltimateCommand{};
+		if (m_ultimateCommand == nullptr)
+			m_ultimateCommand = new UltimateCommand{};
 		m_ultimateCommand->button = button;
 	}
 
 private:
-	Command* m_runRightCommand{};
-	Command* m_runLeftCommand{};
-	Command* m_jumpCommand{};
-	Command* m_attack1Command{};
-	Command* m_attack2Command{};
-	Command* m_rollCommand{};
-	Command* m_ultimateCommand{};
+	Command* m_runRightCommand{ nullptr };
+	Command* m_runLeftCommand{ nullptr };
+	Command* m_jumpCommand{ nullptr };
+	Command* m_attack1Command{ nullptr };
+	Command* m_attack2Command{ nullptr };
+	Command* m_rollCommand{ nullptr };
+	Command* m_ultimateCommand{ nullptr };
 };
